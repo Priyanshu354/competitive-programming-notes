5ae65a552daf3cd07b5e0ae4411f229664f5a6e9
Priyanshu354 <titpriyanshuraj@gmail.com>
2024-10-22 21:42:50 +0530

    update

--- Learning-Tricks/ImportantLearning&Tricks/01-SortedArrayDiffSum.cpp
+++ Learning-Tricks/ImportantLearning&Tricks/01-SortedArrayDiffSum.cpp
@@ -21,8 +21,22 @@ int main()
         
         sum += i * (arr[i] - arr[i - 1]) * (n - i);
     }
+
+    // second way 
+    int total_diff = 0;
+    int sum_before = 0;  // Cumulative sum of elements before the current index
+
+    for (int i = 0; i < n; i++) {
+        // For each arr[i], the difference contribution when arr[i] is added:
+        total_diff += arr[i] * i - sum_before;
+
+        // Update cumulative sum
+        sum_before += arr[i];
+    }
     
     cout << sum << endl;
 
     return 0;
 }
+
+
